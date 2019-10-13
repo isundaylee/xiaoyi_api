b8f80b4ebd05c00566dab65fec8cf535deedd5d6
Jiahao Li <reg@ljh.me>
2019-10-13 13:17:06 -0400

    Implements alert image downloading/decrypting

--- sandbox.py
+++ sandbox.py
@@ -8,7 +8,20 @@ client = xiaoyi.Client()
 
 client.login(os.environ["TEST_ACCOUNT"], os.environ["TEST_ENCODED_PASSWORD"])
 
-from_time = datetime.datetime.now() - datetime.timedelta(days=1)
+from_time = datetime.datetime.now() - datetime.timedelta(days=2)
 to_time = datetime.datetime.now()
+count = 0
 for alert in client.alerts(from_time, to_time):
-    print(datetime.datetime.fromtimestamp(alert.time / 1000))
+    pic_data = alert.get_picture()
+    with open("/tmp/{}.jpg".format(count), "wb") as f:
+        f.write(pic_data)
+
+    print(
+        "{}: {}".format(
+            datetime.datetime.fromtimestamp(alert.time / 1000),
+            "/tmp/{}.jpg".format(count),
+        )
+    )
+
+    count += 1
+
--- xiaoyi/alert.py
+++ xiaoyi/alert.py
@@ -1,5 +1,11 @@
+import cryptography.hazmat.primitives.ciphers
+import cryptography.hazmat.backends
+
+
 class Alert(object):
     def __init__(self, client, data):
+        self.client = client
+
         self.uid = data["uid"]
         self.video_pwd = data["video_pwd"]
         self.sub_type = data["sub_type"]
@@ -11,4 +17,25 @@ class Alert(object):
         self.message = data["message"]
         self.category = data["category"]
         self.pic_pwd = data["pic_pwd"]
+
+    def get_picture(self):
+        return self._get_and_decrypt(self.pic_urls)
+
+    def _get_and_decrypt(self, url):
+        encrypted_data = self.client.session.get(url).content
+        encrypted_data = encrypted_data[4:]
+
+        backend = cryptography.hazmat.backends.default_backend()
+        key = self.pic_pwd.encode()
+        iv = bytes([0] * 16)
+        cipher = cryptography.hazmat.primitives.ciphers.Cipher(
+            cryptography.hazmat.primitives.ciphers.algorithms.AES(key),
+            cryptography.hazmat.primitives.ciphers.modes.ECB(),
+            backend=backend,
+        )
+
+        decryptor = cipher.decryptor()
+        data = decryptor.update(encrypted_data) + decryptor.finalize()
+
+        return data
 
